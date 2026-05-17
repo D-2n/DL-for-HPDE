d49a2167f0d536565362604f1dad65ff5db58531
D-2n <dimitrije.zdrale2@gmail.com>
2026-05-18 00:20:20 +0200

    training larger network cuz better gpu available

--- slurm/train_hypno_st3_decoder_shared.sh
+++ slurm/train_hypno_st3_decoder_shared.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=hypno_st3_dec
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --nodelist=gpu012,gpu013,gpu015,gpu016,gpu017,gpu018
 #SBATCH --cpus-per-gpu=4
 #SBATCH --mem=32G
 #SBATCH --time=48:00:00
